739cb109f1422eb8894d2a6222fe3be0d7bf91d5
trunghieu <trunghieu0451@gmail.com>
2022-09-11 20:32:20 +0700

    SS4

--- SS4/bai2-slider/src/Images.js
+++ SS4/bai2-slider/src/Images.js
@@ -1,29 +1,40 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Images.css";
 
 const Images = () => {
-  const [image, setImage] = useState(4);
+  const [image, setImage] = useState(1);
 
   const clickArrowLeft = () => {
     if (image === 1) {
-      setImage(2);
+      setImage(5);
     } else {
       setImage(image - 1);
     }
   };
-
-  const clickArrowRight = () => {};
+  const clickArrowRight = () => {
+    if (image === 5) {
+      setImage(1);
+    } else {
+      setImage(image + 1);
+    }
+  };
 
   return (
-    <div className="images">
+    <div className="container-images">
       <div className="arrow-left" onClick={clickArrowLeft}>
         &#10094;
       </div>
       <div className="slide">
-        <img alt="" src={`./images/${image}.jpg`} />
+        <img alt=" " src={process.env.PUBLIC_URL + `/images/${image}.jpg`} />
       </div>
       <div className="arrow-right" onClick={clickArrowRight}>
         &#10095;
+      </div>
+
+      <div className="container-dots">
+        {Array.from({length: 5}).map((item, index) => (
+          <div className={image === index + 1 ? "dot active" : "dot"}></div>
+        ))}
       </div>
     </div>
   );
