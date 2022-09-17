b7d1564bd1b72d28532a5a4ac3b6f55f93c719ed
trunghieu <trunghieu0451@gmail.com>
2022-09-17 21:02:01 +0700

    add components

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
